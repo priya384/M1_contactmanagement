fabc7f0f8d1dd9f5106ab0c31dda2902f0cc3ff4
priya384 <62200264+priya384@users.noreply.github.com>
2021-11-21 17:31:38 +0530

    Add files via upload

---
+++ 3_Implemention/calc.h
@@ -0,0 +1,19 @@
+/**
+ * @file calc.h
+ * @author GEA
+ * @brief String based calculator functions
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __CALC_H__
+#define __CALC_H__
+#include "stdlib.h"
+#include "math.h"
+
+int compute(char* str);
+
+#endif //__CALC_H__
